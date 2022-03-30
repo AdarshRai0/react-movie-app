3f0c107527e2ec83627c2c0ef1b28fce08e63f48
Adarshrai0 <adarshrai3011@gmail.com>
2022-03-30 20:09:22 +0530

    reomved app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import axios from "axios";
+import React, { useState } from "react";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
+export const API_KEY = "c934d5d6";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+ 
+`;
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  color: white;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+  @media (max-width: 580px){
+    
+      font-size: 20px;
+      
+    padding : 5px
+  }
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 10px;
+  @media (max-width: 580px){
+    height: 38px;
+    margin: 5px 0;
+  }
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  
+  width: 40%;
+  background-color: white;
+  @media (max-width: 580px){
+    width: 55%;
+    padding: 5px 5px;
+    border-radius: 3px;
+  }
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+  @media (max-width: 580px){
+    width: 22px;
+    height: 22px;
+  }
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+  @media (max-width: 580px){
+    font-size: 10px;
+    margin-left: 5px;
+    font-weight: bold;
+  }
+`;
+
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly; ;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
 
 function App() {
+  const [searchQuery, updateSearchQuery] = useState();
+  const [timeoutId, updateTimeoutId] = useState();
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+  const fetchData = async (searchString) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+    );
+
+    updateMovieList(response.data.Search);
+  };
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId);
+    updateSearchQuery(event.target.value);
+    const timeout = setTimeout(() => fetchData(event.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppName>
+          <MovieImage src="/movie-icon.svg " />
+          React Movies App
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/search-icon.svg" />
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
+      <MovieListContainer>
+      {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <Placeholder src="/movie-icon.svg" />
+        )}
+      </MovieListContainer>
+    </Container>
   );
 }
 
